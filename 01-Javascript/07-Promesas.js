8f0fb7d21583e14c132ae90a3a59a52342711439
franizus <franizus@live.com>
2018-11-05 14:49:39 -0500

    Added Ejercicio JS

--- 01-Javascript/07-Promesas.js
+++ 01-Javascript/07-Promesas.js
@@ -72,3 +72,40 @@ appendFile("07-ejemplo.txt", "\nHola amigos")
   .catch(error => {
     console.log("Catch", error);
   });
+
+
+//EJERCICIO
+const promesaEjercicio = arregloStrings => {
+  return new Promise((resolve, reject) => {
+    const respuestas = [];
+
+    arregloStrings.forEach((string, indice) => {
+      const nombreArchivo = `${indice}-${string}.txt`;
+      const contenido = string;
+      fs.writeFile(nombreArchivo, contenido, err => {
+        const respuesta = {
+          nombreArchivo: nombreArchivo,
+          contenidoArchivo: contenido,
+          error: err
+        };
+        respuestas.push(respuesta);
+
+        const estaCompletoElArreglo = respuestas.length === arregloStrings.length;
+
+        if (estaCompletoElArreglo) {
+          resolve(respuestas);
+        } else {
+          reject();
+        }
+      });
+  });
+};
+
+promesaEjercicio(["A", "B", "C"])
+  .then(respuestaEjercicio => {
+    console.log(respuestaEjercicio);
+  })
+  .catch(() => {
+    console.log("Error");
+  });
+  
